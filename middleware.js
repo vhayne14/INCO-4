7f824d2ea109bbf62da24786041b3296274b1887
vhayne14 <vhayne14@gmail.com>
2021-09-11 12:26:46 +1000

    Added comments on middleware for documentation

--- middleware.js
+++ middleware.js
@@ -1,10 +1,13 @@
 module.exports.redirectToLogin = (req, res, next)=>{
     console.log(req.session.userId);
+    // if req.session.userId === undefined
     if(!req.session.userId){
+        console.log("helo");
         res.clearCookie('mrcoffee_sid')
         res.redirect('/login')
     }
     else{
+        console.log("asd");
         next()
     }
 }
@@ -12,7 +15,7 @@ module.exports.redirectToLogin = (req, res, next)=>{
 
 module.exports.redirectToHome = (req, res, next) =>{
 
-    console.log(req.session.userId);
+    // console.log(req.session.userId);
     if(req.session.userId){
         res.redirect('/')
     }
