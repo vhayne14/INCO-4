121e9899d4587afb3dc2282c1262c3c5b1f302be
vhayne14 <vhayne14@gmail.com>
2021-09-11 23:48:50 +1000

    Added a navbar, and GET routes for profile, schedules, new schedules with their respective ejs files

--- routes/home.js
+++ routes/home.js
@@ -6,30 +6,104 @@ const db = require('../database')
 router.get("/", (req,res)=>{
     
 
-    db.any('SELECT users.id, firstname, lastname, email, day, start_time, end_time FROM users LEFT JOIN schedules ON users.id =  schedules.user_id WHERE users.id = $1;',[req.session.userId])
-    .then((sched)=>{
-        console.log(sched);
-        // res.render("pages/allSched", {
-        //     schedules: sched
-        // })
-        res.send(`
+    db.any('SELECT users.id, firstname, lastname, email, day, start_time, end_time FROM users LEFT JOIN schedules ON users.id =  schedules.user_id;',)
+   .then((sched)=>{
+        res.render("pages/home",{
+            sched
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.send(err);
+        })
+
+   })
+   .catch((err)=>{
+       console.log(err);
+       res.send(err)
+   })
+    
+    
+    
+    // .then((sched)=>{
+    //     console.log(sched);
+    //     // res.render("pages/allSched", {
+    //     //     schedules: sched
+    //     // })
+    //     res.send(`
         
-    <h1> Welcome to homepage</h1>
-    <p> ${sched} </p>
-    <a href ="/logout">Logout</a>
+    // <h1> Welcome to homepage</h1>
+    // <p> ${sched} </p>
+    // <a href ="/logout">Logout</a>
     
     
 
-    `)
+    // `)
 
-    })
-    .catch((err)=>{
-        console.log(error);
-    })
+    // })
+    // .catch((err)=>{
+    //     console.log(error);
+    // })
 
 
 })
 
 
+router.get('/profile',(req,res)=>{
+    db.any('SELECT users.id, firstname, lastname, email, day, start_time, end_time FROM users LEFT JOIN schedules ON users.id =  schedules.user_id WHERE users.id = $1;',[req.session.userId])
+    .then((sched)=>{
+        res.render("pages/home",{
+            sched
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.send(err);
+        })
+
+   })
+   .catch((err)=>{
+       console.log(err);
+       res.send(err)
+   })
+})
+
+router.get('/users/:id',(req,res)=>{
+    db.any('SELECT users.id, firstname, lastname, email, day, start_time, end_time FROM users LEFT JOIN schedules ON users.id =  schedules.user_id WHERE users.id = $1;',[Number(req.params.id)])
+    .then((sched)=>{
+        res.render("pages/home",{
+            sched
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.send(err);
+        })
+
+   })
+   .catch((err)=>{
+       console.log(err);
+       res.send(err)
+   })
+})
+
+
+
+router.get('/new',(req,res)=>{
+    db.any('SELECT users.id, firstname, lastname, email, day, start_time, end_time FROM users LEFT JOIN schedules ON users.id =  schedules.user_id WHERE users.id = $1;',[req.session.userId])
+    .then((sched)=>{
+        res.render("pages/newSched",{
+            sched
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.send(err);
+        })
+
+   })
+   .catch((err)=>{
+       console.log(err);
+       res.send(err)
+   })
+})
+
+//'SELECT users.id, firstname, lastname, email, day, start_time, end_time FROM users LEFT JOIN schedules ON users.id =  schedules.user_id WHERE users.id = $1;',[req.session.userId]
 
 module.exports = router
