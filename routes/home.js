e7db5bb84526ad5676234410c525eda5898c5fe4
vhayne14 <vhayne14@gmail.com>
2021-09-12 14:02:08 +1000

    Re-wrote the /profile route as there's an extra  on the block, compare this to previous version before pulling

--- routes/home.js
+++ routes/home.js
@@ -21,22 +21,19 @@ router.get("/", (req,res) => {
 })
 
 
+
 router.get("/profile",(req,res)=>{
     db.any('SELECT users.id, firstname, lastname, email, day, start_time, end_time FROM users LEFT JOIN schedules ON users.id =  schedules.user_id WHERE users.id = $1;',[req.session.userId])
-    .then((sched => {
+    .then((sched)=>{
         res.render("pages/home",{
             sched
         })
-        .catch((err)=>{
-            console.log(err);
-            res.send(err);
-        })
-   })
-   .catch((err)=>{
-       console.log(err);
-       res.send(err)
-   }))
-  })
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.send(err)
+    })
+})
 
 router.get('/users/:id',(req,res)=>{
     db.any('SELECT users.id, firstname, lastname, email, day, start_time, end_time FROM users LEFT JOIN schedules ON users.id =  schedules.user_id WHERE users.id = $1;',[Number(req.params.id)])
