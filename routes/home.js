264cf5fc720bb17bfbb25a8c838f003cc8480227
TatiTait <ladytatitait@gmail.com>
2021-09-12 17:26:49 +1000

    added POST route for new schedule but time trows error

--- routes/home.js
+++ routes/home.js
@@ -73,6 +73,18 @@ router.get('/new',(req,res)=>{
    })
 })
 
+router.post('/new', (req, res) => {
+  db.none('INSERT INTO schedules (user_id, day, start_time, end_time) VALUES ($1, $2, $3, $4);',
+  [req.body.user_id, req.body.day, req.body.start_time, req.body.end_time])
+  .then(()=>{
+      res.render("pages/home");
+  })
+  .catch((err)=>{
+      console.log(err);
+      res.send(err)
+  })
+})
+
 //'SELECT users.id, firstname, lastname, email, day, start_time, end_time FROM users LEFT JOIN schedules ON users.id =  schedules.user_id WHERE users.id = $1;',[req.session.userId]
 
 module.exports = router
